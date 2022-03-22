2b4e1dc22fe50792049a56383fc6c1da597611b8
Tarsis de Lima <br.tmvdl@gmail.com>
2022-03-21 22:41:51 -0300

    #Login: Implementing basic functions

--- login/main.go
+++ login/main.go
@@ -1,8 +1,28 @@
 
 package main
 
-import "fmt"
+import "os"
+import "net/http"
+
+func check(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func RequestHandler() (string, error) {
+	return "{}", nil
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		response, err := RequestHandler()
+		check(err)
+		w.Write([]byte(response))
+	})
+
+	host := os.Getenv("HOSTNAME")
+	port := os.Getenv("PORT")
+	err := http.ListenAndServe(host + ":" + port, nil)
+	check(err)
 }
